17a60af195759820269c0101fe2c10ea96058c94
Tharusha Dilshan Bandara <tharushadil0@gmail.com>
2025-06-18 11:18:11 +0530

    Test: Simple React component to debug deployment

---
+++ App.simple.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ color: 'white', padding: '20px', backgroundColor: '#0f172a' }}>
+      <h1>Portfolio Test - React is Working!</h1>
+      <p>If you can see this, React is mounting correctly.</p>
+    </div>
+  );
+};
+
+export default App;
